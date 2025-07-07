566fd7db7685a4e244e2d7aaf41c5bf54879120f
Michał Bronicki <michalbronicki85@gmail.com>
2025-07-07 11:41:54 +0200

    fix: build

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -2,11 +2,34 @@ import type { Endpoints } from "@octokit/types";
 import { type ColorsMap } from "@/types/Colors";
 import type { AstroGlobal } from "astro";
 
+import { Octokit } from "@octokit/core";
+
+const repos = [
+  "portfolio-astro",
+  "weather-app",
+  "calculator-app",
+  "portfolio",
+  "event-deadline-viewer",
+];
+
+const octokit = new Octokit({
+  auth: import.meta.env.GITHUB_AUTH_KEY,
+});
+
 export type Repo = Endpoints["GET /repos/{owner}/{repo}"]["response"]["data"];
 
 export async function fetchRepos(Astro: AstroGlobal): Promise<Repo[]> {
-  const res = await fetch(new URL("/api/github", Astro.url));
-  return await res.json();
+  const data = await Promise.all(
+    repos.map((name) =>
+      octokit
+        .request("GET /repos/{owner}/{repo}", {
+          owner: "xchrl",
+          repo: name,
+        })
+        .then((res) => res.data)
+    )
+  );
+  return data;
 }
 
 export async function fetchLanguageColors(): Promise<ColorsMap> {
